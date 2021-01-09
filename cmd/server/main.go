2a3013af82f8c65952c9d7a6c463f8a9c10ef814
thisisaaronland <devnull@localhost>
2021-01-08 16:57:24 -0800

    stubs for CORS headers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"github.com/aaronland/go-http-server"
+	_ "github.com/rs/cors"
 	"github.com/sfomuseum/go-edtf-http/api"
 	"github.com/sfomuseum/go-flags/flagset"
 	"log"
@@ -17,14 +17,25 @@ func main() {
 	fs := flagset.NewFlagSet("server")
 
 	server_uri := fs.String("server-uri", "http://localhost:8080", "A valid aaronland/go-http-server URI.")
+	enable_cors := fs.Bool("enable-cors", false, "Enable CORS headers for API responses")
+
+	enable_parse_api := fs.Bool("enable-parse-api", true, "Enable the /api/parse endpoint")
+	enable_valid_api := fs.Bool("enable-valid-api", true, "Enable the /api/valid endpoint")
+	enable_matches_api := fs.Bool("enable-matches-api", true, "Enable the /api/matches endpoint")
 
 	fs.Usage = func() {
 		fmt.Fprintf(os.Stderr, "HTTP server for exposing sfomuseum/go-edtf-http handlers.\n")
 		fmt.Fprintf(os.Stderr, "Usage:\n\t %s [options]\n", os.Args[0])
-		flag.PrintDefaults()
+		fs.PrintDefaults()
 	}
 
 	flagset.Parse(fs)
+
+	err := flagset.SetFlagsFromEnvVars(fs, "EDTF")
+
+	if err != nil {
+		log.Fatalf("Failed to set flags from environment variables, %v", err)
+	}
 
 	ctx := context.Background()
 
@@ -34,29 +45,52 @@ func main() {
 		log.Fatalf("Failed to create new server, %v", err)
 	}
 
-	api_parse_handler, err := api.ParseHandler()
-
-	if err != nil {
-		log.Fatalf("Failed to API parse handler, %v", err)
-	}
-
-	api_valid_handler, err := api.IsValidHandler()
-
-	if err != nil {
-		log.Fatalf("Failed to API is valid handler, %v", err)
-	}
-
-	api_matches_handler, err := api.MatchesHandler()
-
-	if err != nil {
-		log.Fatalf("Failed to API is matches handler, %v", err)
-	}
-
 	mux := http.NewServeMux()
 
-	mux.Handle("/api/parse", api_parse_handler)
-	mux.Handle("/api/valid", api_valid_handler)
-	mux.Handle("/api/matches", api_matches_handler)
+	if *enable_parse_api {
+
+		api_parse_handler, err := api.ParseHandler()
+
+		if err != nil {
+			log.Fatalf("Failed to API parse handler, %v", err)
+		}
+
+		if *enable_cors {
+			// api_parse_handler = c.Handler(api_parse_handler)
+		}
+
+		mux.Handle("/api/parse", api_parse_handler)
+	}
+
+	if *enable_valid_api {
+
+		api_valid_handler, err := api.IsValidHandler()
+
+		if err != nil {
+			log.Fatalf("Failed to API is valid handler, %v", err)
+		}
+
+		if *enable_cors {
+			// api_valid_handler = c.Handler(api_valid_handler)
+		}
+
+		mux.Handle("/api/valid", api_valid_handler)
+	}
+
+	if *enable_matches_api {
+
+		api_matches_handler, err := api.MatchesHandler()
+
+		if err != nil {
+			log.Fatalf("Failed to API is matches handler, %v", err)
+		}
+
+		if *enable_cors {
+			// api_matches_handler = c.Handler(api_matches_handler)
+		}
+
+		mux.Handle("/api/matches", api_matches_handler)
+	}
 
 	log.Printf("Listening on %s", s.Address())
 	err = s.ListenAndServe(ctx, mux)
